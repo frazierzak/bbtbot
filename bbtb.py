3bee579b3c5d9bcb49cab668ec6938286314b6c2
zuffdaddy <Zakfrazier@gmail.com>
2016-07-04 14:05:03 -0400

    Update bbtb.py

--- bbtb.py
+++ bbtb.py
@@ -14,63 +14,63 @@ from config_bot import *
 #Check for config_bot.py
 print "Checking for config_bot.py..."
 if not  os.path.isfile("config_bot.py"):
-    print "You must create a config file with your username and password."
-    print "Please see config_skel.py"
-    exit(1)
+  print "You must create a config file with your username and password."
+  print "Please see config_skel.py"
+  exit(1)
 
 #Define User_Agent and Bot Name
 user_agent = ("BBTB by zuffdaddy 0.1")
 r = praw.Reddit(user_agent = user_agent)
 
 while True:
-    try:
-        #Log In with Bot
-        print "Logging in..."
-        r.login(REDDIT_USERNAME, REDDIT_PASS, disable_warning=True)
+  try:
+    #Log In with Bot
+    print "Logging in..."
+    r.login(REDDIT_USERNAME, REDDIT_PASS, disable_warning=True)
 
-        #Check for comments_replied_to.txt and create if it doesn't exist
-        if not os.path.isfile("comments_replied_to.txt"):
-            comments_replied_to = []
+    #Check for comments_replied_to.txt and create if it doesn't exist
+    if not os.path.isfile("comments_replied_to.txt"):
+        comments_replied_to = []
 
-        # If we have run the code before, load the list of comments we have replied to
-        else:
-            # Read the file into a list and remove any empty values
-            with open("comments_replied_to.txt", "r") as f:
-                comments_replied_to = f.read()
-                comments_replied_to = comments_replied_to.split("\n")
-                comments_replied_to = filter(None, comments_replied_to)
+    # If we have run the code before, load the list of comments we have replied to
+    else:
+      # Read the file into a list and remove any empty values
+      with open("comments_replied_to.txt", "r") as f:
+        comments_replied_to = f.read()
+        comments_replied_to = comments_replied_to.split("\n")
+        comments_replied_to = filter(None, comments_replied_to)
 
-        #Choose Subreddit
-        subreddit = r.get_subreddit("bigbrother")
-        print "Loading Subreddit", subreddit
+    #Choose Subreddit
+    subreddit = r.get_subreddit("bigbrother")
+    print "Loading Subreddit", subreddit
 
-        #Get Top 2 Submissions
-        print "Finding top 2 submissions in", subreddit
-        for submission in subreddit.get_hot(limit = 2):
-            print 'Parsing thread:', submission.title
-            submission.replace_more_comments(limit=None, threshold=0)
-            all_comments = praw.helpers.flatten_tree(submission.comments)
-            for comment in all_comments:
-                if re.search("!BBT", comment.body) and comment.id not in comments_replied_to:
-                    print 'Phrase found in comment id:', comment.link_id
-                    comment_time = comment.created_utc
-                    print 'Comment created at:', comment_time
-                    arrow_time = arrow.get(comment_time)
-                    print 'Converting to BBT'
-                    pst_time = arrow_time.to('US/Pacific')
-                    pst_time = pst_time.format('h:mma - MMM D')
-                    print 'Current BBT is:', pst_time
-                    
-                    print 'Replying with BBT'
-                    comment.reply(pst_time) #reply to comment
-                    comments_replied_to.append(comment.id) #add comment link id to comments replied to
+    #Get Top 2 Submissions
+    print "Finding top 2 submissions in", subreddit
+    for submission in subreddit.get_hot(limit = 2):
+      print 'Parsing thread:', submission.title
+      submission.replace_more_comments(limit=None, threshold=0)
+      all_comments = praw.helpers.flatten_tree(submission.comments)
+      for comment in all_comments:
+        if re.search("!BBT", comment.body) and comment.id not in comments_replied_to:
+          print 'Phrase found in comment id:', comment.link_id
+          comment_time = comment.created_utc
+          print 'Comment created at:', comment_time
+          arrow_time = arrow.get(comment_time)
+          print 'Converting to BBT'
+          pst_time = arrow_time.to('US/Pacific')
+          pst_time = pst_time.format('h:mma - MMM D')
+          print 'Current BBT is:', pst_time
+          
+          print 'Replying with BBT'
+          comment.reply(pst_time) #reply to comment
+          comments_replied_to.append(comment.id) #add comment link id to comments replied to
 
-        # Write our updated list back to the file
-        with open("comments_replied_to.txt", "w") as f:
-            for comment_id in comments_replied_to:
-                f.write(comment_id + "\n")
-        print 'Done! Waiting for a minute before I go postin again!'
-        #time.sleep(10) # delays for 5 seconds
-    except:
-        print 'Error! Restarting the script'
-    time.sleep(30)
+    # Write our updated list back to the file
+    with open("comments_replied_to.txt", "w") as f:
+      for comment_id in comments_replied_to:
+        f.write(comment_id + "\n")
+    print 'Done! Waiting for a minute before I go postin again!'
+    #time.sleep(10) # delays for 5 seconds
+  except:
+    print 'Error! Restarting the script'
+  time.sleep(30)
